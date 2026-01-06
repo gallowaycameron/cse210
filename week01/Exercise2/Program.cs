ef5e43dea00bfbc398bad57b255812a3d0a5f4d5
gallowaycameron <cgalloway@byupathway.edu>
2026-01-06 10:40:42 -0700

    Added grade code

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+        int number = int.Parse(gradePercentage);
+
+        string letter = "";
+        string notice = "";
+
+        if (number >= 70)
+        {
+            notice = "Congratulations, you are passing!";
+
+            if (number >= 90)
+            {
+                letter = "A";
+            }
+
+            else if (number >= 80)
+            {
+                letter = "B";
+            }
+
+            else
+            {
+                letter = "C";
+            }
+        }
+
+        else
+        {
+            notice = "Sorry, you are not passing. Try a little harder";
+
+            if (number >= 60)
+            {
+                letter = "D";
+            }
+
+            else
+            {
+                letter = "F";
+            }
+
+        }
+
+        Console.WriteLine($"Your grade is {letter}. \n{notice}");
     }
 }
