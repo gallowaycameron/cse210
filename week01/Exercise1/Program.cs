a99d7eaa5aab0c3b04b2ab454bcb86176a322e84
gallowaycameron <cgalloway@byupathway.edu>
2026-01-06 10:03:09 -0700

    added last name and first name input, and display them

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string firstname = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
     }
 }
+
+// Teachers example
+
+// using System;
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.Write("What is your first name? ");
+//         string first = Console.ReadLine();
+
+//         Console.Write("What is your last name? ");
+//         string last = Console.ReadLine();
+
+//         Console.WriteLine($"Your name is {last}, {first} {last}.");
+//     }
+// }
