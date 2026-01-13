9f27d23e5ec0d602223f6566b21ac551640e912c
gallowaycameron <cgalloway@byupathway.edu>
2026-01-13 09:02:32 -0700

    Made Resume program

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Mirosoft";
+        job1._startYear = 2002;
+        job1._endYear = 2005;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Developer";
+        job2._company = "Apple";
+        job2._startYear = 2005;
+        job2._endYear = 2009;
+
+        Resume resume = new Resume();
+        resume._name = "Cameron Galloway";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        resume.Display();
     }
 }
